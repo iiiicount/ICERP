6f75d3641e76a6b7d6bce75a6ac95b6a23cd9ae0
GyuHyeok Lim <gyuhyeoklim@GyuHyeokui-MacBookAir.local>
2023-01-05 20:27:33 +0900

    first commit

---
+++ src/main/java/com/count/icount/ICountApplication.java
@@ -0,0 +1,13 @@
+package com.count.icount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ICountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ICountApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/count/icount/ICountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.count.icount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ICountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
