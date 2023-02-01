49e853182eba668e0e0f68f43d2d80cdc18b93a2
gomcool17 <gomcool17@naver.com>
2023-02-01 20:07:54 +0900

    [ADD] entity

---
+++ src/main/java/com/count/icount/trade/Purchase/Model/Entity/PurchaseDetail.java
@@ -0,0 +1,24 @@
+package com.count.icount.Trade.Purchase.Model.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PurchaseDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String product;
+    private int quantity;
+    private String beego;
+}
