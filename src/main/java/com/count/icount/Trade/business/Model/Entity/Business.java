0fe411038737d58b0ba1b9c08287174b0f3b554c
GyuHyeok Lim <gyuhyeoklim@GyuHyeokui-MacBookAir.local>
2023-02-01 20:14:21 +0900

    [ADD]: entity

---
+++ src/main/java/com/count/icount/Trade/business/Model/Entity/Business.java
@@ -0,0 +1,35 @@
+package com.count.icount.trade.business.Model.Entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Business {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String businessNum;
+
+    private String telephone;
+
+    private String address;
+
+    private String accountNum;
+
+    private Long bankId;
+
+
+
+}
