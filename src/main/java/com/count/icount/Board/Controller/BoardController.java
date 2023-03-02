f6843b8ee08267f7edcd5508c29b5b604b0fc294
kyungmin123 <kimkm3972@naver.com>
2023-03-02 20:50:46 +0900

    post

--- src/main/java/com/count/icount/Board/Controller/BoardController.java
+++ src/main/java/com/count/icount/Board/Controller/BoardController.java
@@ -3,6 +3,7 @@ package com.count.icount.Board.Controller;
 import com.count.icount.Board.Dto.BoardRequestDto;
 import com.count.icount.Board.Dto.BoardResponseDto;
 import com.count.icount.Board.Service.BoardService;
+import com.count.icount.model.Dto.Response;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
@@ -22,17 +23,16 @@ public class BoardController {
         return ResponseEntity.ok(boardService.get());
     }
 
-//    @GetMapping("/boards")
-//    public ResponseEntity<BoardResponseDto> get Boards (BoardRequestDto boardRequestDto) {
-//        return ResponseEntity.ok(boardService.gets(boardRequestDto));
-//    }
-
-//    @PostMapping("")
-//    public ResponseEntity<BoardResponseDto> saveBoard(@RequestBody BoardRequestDto boardRequestDto)  {
-//        // log 사용법
-//        // log.info("Board save Controller : ");
-//        return ResponseEntity.ok(boardService.save(boardRequestDto));
-//    }
+    @GetMapping("/writer/{writer}")
+    public ResponseEntity<List<BoardResponseDto>> getBoardByWriter(@PathVariable("writer") String writer) {
+        return ResponseEntity.ok(boardService.getBoardByWriter(writer));
+    }
+    @PostMapping("")
+    public ResponseEntity<BoardResponseDto> saveBoard(@RequestBody BoardRequestDto boardRequestDto)  {
+        // log 사용법
+        // log.info("Board save Controller : ");
+        return ResponseEntity.ok(boardService.save(boardRequestDto));
+    }
 //
 //    @DeleteMapping("")
 //    public ResponseEntity<BoardResponseDto> deleteBoard (BoardRequestDto boardRequestDto) {
