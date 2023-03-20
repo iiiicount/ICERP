532a144cd7f4469e6bf5f94bcf8efa24b1e1b24b
kyungmin123 <kimkm3972@naver.com>
2023-03-20 20:51:44 +0900

    주석 제거

--- src/main/java/com/count/icount/Board/Controller/BoardController.java
+++ src/main/java/com/count/icount/Board/Controller/BoardController.java
@@ -43,10 +43,4 @@ public class BoardController {
     public ResponseEntity<BoardResponseDto> modify (@PathVariable("id") long id, @RequestBody BoardRequestDto boardRequestDto) {
         return ResponseEntity.ok(boardService.modify(id, boardRequestDto));
     }
-//
-//    @PutMapping("/content/id/{id}")
-//    public ResponseEntity<BoardResponseDto> updateBoardByContent (@PathVariable("id") long id, String content) {
-//        return ResponseEntity.ok(boardService.updateContent(id, content));
-//    }
-
 }
