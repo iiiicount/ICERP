18ebb44dd7e8ff9014b547f19c652a38e5c0d8bd
venandi48 <bow999@naver.com>
2023-04-23 19:12:12 +0900

    [FIX] product fk

--- src/main/java/com/count/icount/company/Model/Entity/Company.java
+++ src/main/java/com/count/icount/company/Model/Entity/Company.java
@@ -1,9 +1,6 @@
 package com.count.icount.company.Model.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,6 +13,7 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Company {
     @Id
+    @Column(name="com_code")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long comCode;
     private String businessNumber;
