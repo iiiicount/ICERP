4a916e0b8ddae9d9e0a754400e02a94077dc85cd
venandi48 <bow999@naver.com>
2023-05-01 16:01:58 +0900

    [ADD] basic companyProductList API

--- src/main/java/com/count/icount/Trade/product/Controller/ProductController.java
+++ src/main/java/com/count/icount/Trade/product/Controller/ProductController.java
@@ -3,10 +3,17 @@ package com.count.icount.Trade.product.Controller;
 
 import com.count.icount.Trade.product.Model.Dto.ProductRequestDto;
 import com.count.icount.Trade.product.Model.Dto.ProductResponseDto;
+import com.count.icount.Trade.product.Model.Dto.GetProductResponseDto;
 import com.count.icount.Trade.product.Service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,12 +24,20 @@ public class ProductController {
 
 
     @PostMapping
-    public ResponseEntity<ProductResponseDto> saveProduct(@RequestBody ProductRequestDto product){
-        if(product.getId() != null) {
+    public ResponseEntity<ProductResponseDto> saveProduct(@RequestBody ProductRequestDto product) {
+        if (product.getId() != null) {
             return ResponseEntity.badRequest().build();
         }
 
         var result = productService.saveProduct(product);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/com-code/{comCode}")
+    public ResponseEntity<List<GetProductResponseDto>> getCompanyProducts(@PathVariable("comCode") Long comCode,
+                                                                   @PageableDefault(size = 20, sort = {"enrollDt"}, direction = Sort.Direction.DESC) Pageable page) {
+        var result = productService.getProducts(comCode, page);
+
         return ResponseEntity.ok(result);
     }
 
--- src/main/java/com/count/icount/Trade/product/Service/ProductService.java
+++ src/main/java/com/count/icount/Trade/product/Service/ProductService.java
@@ -2,11 +2,17 @@ package com.count.icount.Trade.product.Service;
 
 import com.count.icount.Trade.product.Model.Dto.ProductRequestDto;
 import com.count.icount.Trade.product.Model.Dto.ProductResponseDto;
+import com.count.icount.Trade.product.Model.Dto.GetProductResponseDto;
 import com.count.icount.Trade.product.Model.Entity.Product;
 import com.count.icount.Trade.product.Repository.ProductRepository;
 import com.count.icount.company.Model.Entity.Company;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,12 +20,33 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
+    @Transactional
     public ProductResponseDto saveProduct(ProductRequestDto product){
-        // 이 과정 반복될거같으니까 고민해보기
-        Company company = new Company();
-        company.setComCode(product.getComCode());
-
+        var company = validateCompany(product.getComCode());
         Product newProduct = Product.of(product, company);
+
         return ProductResponseDto.of(productRepository.save(newProduct));
+    }
+
+    @Transactional(readOnly = true)
+    public List<GetProductResponseDto> getProducts(long comCode, Pageable page) {
+        List<GetProductResponseDto> result = new ArrayList<>();
+        var products = productRepository.findAllByCompany_ComCode(comCode, page);
+
+        if(products.size() > 0) {
+            for(var product : products) {
+                var response = GetProductResponseDto.of(product);
+                result.add(response);
+            }
+        }
+        return result;
+    }
+
+    private Company validateCompany(long comCode) {
+        // 임시코드. 수정필요.
+        Company company = new Company();
+        company.setComCode(comCode);
+
+        return company;
     }
 }
